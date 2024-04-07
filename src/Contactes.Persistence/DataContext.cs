3b2d9654c01290588e0d6f89aa527102abc59463
milioman3 <79803529+milioman3@users.noreply.github.com>
2024-04-07 20:34:18 -0300

    init
    This is the initialization of the project

---
+++ src/Contactes.Persistence/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Contactes.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contactes.Persistence
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options) 
+        {
+        }
+        public DbSet<Contacte> Contactes { get; set; }
+    }
+}
